83427bf5a0b95500552b86108cde681c2e9322d5
martin <m.schaumann@mscloud.uk>
2025-02-28 17:04:35 +0100

    ...

--- nixos/MediaServer/pkgs.nix
+++ nixos/MediaServer/pkgs.nix
@@ -2,13 +2,15 @@
   pkgs,
   inputs,
   ...
-}: {
+}:
+{
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     gcc
     cifs-utils
     nodejs_23
     go
+    nerdfetch
     unzip
     fzf
     nil
