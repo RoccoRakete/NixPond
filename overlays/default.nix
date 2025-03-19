089c2fdbee6d0879676ec92a0ddb0d31077e6d27
martin <m.schaumann@mscloud.uk>
2025-03-19 22:22:56 +0100

    ...

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,15 +15,15 @@
         hash = "sha256-VIygMzCPTKzLr47bG1DYy/zj0OxsjGcms0G1BkI/TEI=";
       };
     };
-    jellyseerr = prev.jellyseerr.overrideAttrs (finalAttrs: {
+    jellyseerr = prev.jellyseerr.overrideAttrs {
       version = "2.5.1";
       src = prev.fetchFromGitHub {
         owner = "Fallenbagel";
-        tag = "v${finalAttrs.version}";
+        tag = "v2.5.1";
         repo = "jellyseerr";
         hash = "sha256-vAMuiHcf13CDyOB0k36DXUk+i6K6h/R7dmBLJsMkzNA=";
       };
-    });
+    };
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
